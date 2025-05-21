81d7395d64e77d58798ddf35d253ae5b70998ee3
BuildTools <unconfigured@null.spigotmc.org>
2025-05-21 23:31:40 +0200

    Fix JSON URL for questions

--- lpi.sh
+++ lpi.sh
@@ -100,7 +100,7 @@ echo "<biw>{{ BR-scissors }} Setting environment ...</biw>\n"
 which jq &> /dev/null || fail 1 "jq not found, please install and try again."
 
 ## Fetch data
-LPI_QUESTIONS_URL="https://raw.githubusercontent.com/Noam-Alum/lpi_010_160_exam/refs/heads/main/lpi/lpi_questions.json"
+LPI_QUESTIONS_URL="https://raw.githubusercontent.com/JernicVR/lpi_010_160_theoretical_exam/main/lpi/lpi_exercise_questions.json"
 readonly LPI_QUESTIONS_URL
 
 echo "$INFO_BULLET <biw>Validating url.</biw>"
